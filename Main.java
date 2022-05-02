f71c3d5807f2717c5bf7718515e08168ba74de6e
Ilya0567 <79191585+Ilya0567@users.noreply.github.com>
2022-05-02 14:45:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package lesson17;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Илья";
+        int age = 24;
+        System.out.println("Привет, " + name + "!");
+        System.out.println("До ста лет тебе осталось " + (100 - age) + " лет.");
+        System.out.println("Hellow, Ilya!\n You have " + (100 - age) + " year to 100 year");
+    }
+}
+
